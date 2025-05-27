d41a91a9afcd41c4ff68031b46e376dc4b25c10c
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-05-27 16:54:07 +0900

    メモ記入画面から渡された値をTextView上に表示させる処理を追加 DBへの登録処理をメモ表示画面ファイルに記入中

--- app/src/main/java/com/example/clipmemoapp/MemoDisplay.java
+++ app/src/main/java/com/example/clipmemoapp/MemoDisplay.java
@@ -1,8 +1,10 @@
 package com.example.clipmemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +12,67 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Date;
+
+import io.realm.Realm;
+
 public class MemoDisplay extends AppCompatActivity {
 
+    Realm mRealm;
     private Button backBtn;
+    private TextView TitleView;
+    private TextView TextView;
+    public String TitleDisplay;
+    public String TextDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_memo_display);
+        Intent intent = getIntent();
+        TitleDisplay = intent.getStringExtra("MEMO_TITLE");
+        TextDisplay = intent.getStringExtra("MEMO_TEXT");
+        mRealm = Realm.getDefaultInstance();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TitleView = findViewById(R.id.titleView);
+        TextView = findViewById(R.id.textView);
+        TitleView.setText(TitleDisplay);
+        TextView.setText(TextDisplay);
         backBtn = findViewById(R.id.returnButton);
-
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                mRealm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        memo memo
+                                = realm.createObject(memo.class,0);
+                        memo.title = TitleDisplay;
+                        memo.text = TextDisplay;
+                        memo.category = 0;
+                        memo.favorite_flag = 0;
+                        memo.block_flag = 0;
+                        memo.password_needed_flag = 0;
+                        memo.memo_delete_flag = 0;
+                        memo.made_date = new Date();
+                        memo.updated_date = new Date();
+                    }
+                });
+                Intent intent = new Intent(MemoDisplay.this,MainActivity.class);
+                startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mRealm.close();
     }
 }
