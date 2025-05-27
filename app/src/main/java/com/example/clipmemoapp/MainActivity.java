3607c086cb1a9da20186ce56fe36a7a31cef35ad
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-05-27 12:00:53 +0900

    初期画面の仮作成（ボタンの動作+画面遷移のチェック）

--- app/src/main/java/com/example/clipmemoapp/MainActivity.java
+++ app/src/main/java/com/example/clipmemoapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.clipmemoapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button importBtn,viewBtn,userBtn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,31 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        importBtn = findViewById(R.id.memo_import_Button);
+        viewBtn = findViewById(R.id.memo_view_Button);
+        userBtn = findViewById(R.id.userButton);
+
+        importBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MemoImport.class);
+                startActivity(intent);
+            }
+        });
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MemoDisplay.class);
+                startActivity(intent);
+            }
+        });
+        userBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,UserMenu.class);
+                startActivity(intent);
+            }
         });
     }
 }
