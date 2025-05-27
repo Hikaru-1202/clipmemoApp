8a5f30f871f696c48f2e98e51af724d5f4df876f
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-05-27 16:51:52 +0900

    メモ記入画面内のテキストボックスに入力した値を遷移先のメモ表示画面に渡す処理を追加

--- app/src/main/java/com/example/clipmemoapp/MemoImport.java
+++ app/src/main/java/com/example/clipmemoapp/MemoImport.java
@@ -1,8 +1,10 @@
 package com.example.clipmemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MemoImport extends AppCompatActivity{
-
     private Button backBtn;
+    private EditText editTitle;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_memo_import);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         backBtn = findViewById(R.id.returnButton);
-
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
+                editTitle = findViewById(R.id.editTitle);
+                editText = findViewById(R.id.editText);
+                intent.putExtra("MEMO_TITLE",editTitle.getText().toString());
+                intent.putExtra("MEMO_TEXT",editText.getText().toString());
+                startActivity(intent);
             }
         });
     }
