1e9d7ef9beebc1b8576a35c5cca48c55e72062c0
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-06-02 10:28:41 +0900

    ボタンとDBの機能を追加

--- app/src/main/java/com/example/clipmemoapp/MainActivity.java
+++ app/src/main/java/com/example/clipmemoapp/MainActivity.java
@@ -9,12 +9,14 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
+    Realm mRealm;
     private Button importBtn;
     private Button viewBtn;
     private Button userBtn;
-
-    private Button pre_deleteBtn;
+    private String[] Memo_Data = {};
 
 
     @SuppressLint("MissingInflatedId")
@@ -23,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mRealm = Realm.getDefaultInstance();
+
         importBtn = findViewById(R.id.memo_import_Button);
         viewBtn = findViewById(R.id.memo_view_Button);
         userBtn = findViewById(R.id.userButton);
-        pre_deleteBtn = findViewById(R.id.delete_Button);
+
+        //月曜日（6/2）にレルムの処理追加
 
         importBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,11 +55,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        pre_deleteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
-            }
-        });
+        mRealm.close();
     }
 }
