6459d962dcfd15222915fc4df8a71b88d75c99e2
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-06-02 10:22:40 +0900

    テキストボックス内にヒント情報を表示

--- app/src/main/java/com/example/clipmemoapp/MemoImport.java
+++ app/src/main/java/com/example/clipmemoapp/MemoImport.java
@@ -1,18 +1,14 @@
 package com.example.clipmemoapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Date;
 
@@ -21,35 +17,50 @@ import io.realm.Realm;
 public class MemoImport extends AppCompatActivity {
     Realm mRealm;
     private Button backBtn;
-    private EditText E_Title;
-    private EditText E_Text;
+    private EditText I_Title;
+    private EditText I_Text;
     private String G_Title;
     private String G_Text;
+    private long newId = 0;
+    private long ID_NUM;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_memo_import);
-
         mRealm = Realm.getDefaultInstance();
-        backBtn = findViewById(R.id.returnButton);
 
+        backBtn = findViewById(R.id.returnButton);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                E_Title = findViewById(R.id.editTitle);
-                E_Text = findViewById(R.id.editText);
-                G_Title = E_Title.getText().toString();
-                G_Text = E_Text.getText().toString();
+                I_Title = findViewById(R.id.importTitle);
+                I_Text = findViewById(R.id.importText);
+                G_Title = I_Title.getText().toString();
+                G_Text = I_Text.getText().toString();
 
                 mRealm.executeTransactionAsync(new Realm.Transaction() {
                     @Override
                     public void execute(Realm realm) {
+                        Number max = realm.where(Memo.class).max("memo_id");
+                        newId = 0;
+                        if (max != null){
+                            newId = max.longValue() + 1;
+                        }
                         Memo memo
-                                = realm.createObject(Memo.class,0);
-                        memo.title = G_Title;
-                        memo.text = G_Text;
+                                = realm.createObject(Memo.class, newId);
+                        if (G_Title.isEmpty()){
+                            memo.title = "No Title";
+                        }else {
+                            memo.title = G_Title;
+                        }
+                        if (G_Text.isEmpty()){
+                            memo.text = "No Text";
+                        }else {
+                            memo.text = G_Text;
+                        }
                         memo.category = 0;
                         memo.favorite_flag = 0;
                         memo.block_flag = 0;
@@ -57,10 +68,12 @@ public class MemoImport extends AppCompatActivity {
                         memo.memo_delete_flag = 0;
                         memo.made_date = new Date();
                         memo.updated_date = new Date();
+
+                        Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
+                        intent.putExtra("KeyNUM", newId);
+                        startActivity(intent);
                     }
                 });
-                Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
-                startActivity(intent);
             }
         });
     }
