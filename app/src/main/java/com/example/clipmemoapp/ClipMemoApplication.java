279d0b398b37315e494f99c7635632fb1befcb35
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-06-09 17:56:41 +0900

    初回起動時の動作追加（予定）

--- app/src/main/java/com/example/clipmemoapp/ClipMemoApplication.java
+++ app/src/main/java/com/example/clipmemoapp/ClipMemoApplication.java
@@ -2,9 +2,12 @@ package com.example.clipmemoapp;
 
 import android.app.Application;
 
+import java.util.Date;
+
 import io.realm.Realm;
 
 public class ClipMemoApplication extends Application {
+//    private long newId;
     @Override
     public void onCreate() {
         super.onCreate();
