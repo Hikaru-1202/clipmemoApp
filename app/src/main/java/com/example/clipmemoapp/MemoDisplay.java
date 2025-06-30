b0ed8a0612ceb33a98276b4c4751476cdcb1799d
Hikaru-1202 <hikaru.suzuki@nsw.co.jp>
2025-06-30 17:26:17 +0900

    メモ表示画面、メモ記入画面の追加機能作成（保存ボタン追加、戻るボタンの機能改善）

--- app/src/main/java/com/example/clipmemoapp/MemoDisplay.java
+++ app/src/main/java/com/example/clipmemoapp/MemoDisplay.java
@@ -1,16 +1,28 @@
 package com.example.clipmemoapp;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.gesture.GestureOverlayView;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.GestureDetector;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.fragment.app.DialogFragment;
 
 import io.realm.Realm;
 
@@ -21,6 +33,18 @@ public class MemoDisplay extends AppCompatActivity {
     private TextView TitleView;
     private TextView TextView;
     private long ID_NUM = 0;
+    private int memoBlock = 0;
+    private String title;
+    private String text;
+    private int pass_flag;
+    private int block_flag;
+    private int favorite_flag;
+
+    private final int flag = 1;
+    private final int unflag = 0;
+
+
+//    GestureDetector mDetector;
 
 
     @SuppressLint("MissingInflatedId")
@@ -30,15 +54,18 @@ public class MemoDisplay extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_memo_display);
 
+        registerForContextMenu(findViewById(R.id.displaytextView));
+//        mDetector = new GestureDetector(this, (GestureDetector.OnGestureListener) this);
+
         ID_NUM = getIntent().getLongExtra("KeyNUM",0);
         Log.v("MY_LOG","Display:"+String.valueOf(ID_NUM));
 
-        ((TextView) findViewById(R.id.textView)).setMovementMethod(new ScrollingMovementMethod());
+        ((TextView) findViewById(R.id.displaytextView)).setMovementMethod(new ScrollingMovementMethod());
 
         mRealm = Realm.getDefaultInstance();
 
         TitleView = findViewById(R.id.titleView);
-        TextView = findViewById(R.id.textView);
+        TextView = findViewById(R.id.displaytextView);
 
         mRealm.executeTransactionAsync(new Realm.Transaction() {
             @Override
@@ -46,13 +73,18 @@ public class MemoDisplay extends AppCompatActivity {
                 Memo memo = realm.where(Memo.class)
                         .equalTo("memo_id",ID_NUM).findFirst();
 
-                String title = memo.title;
-                String text = memo.text;
+                title = memo.title;
+                text = memo.text;
+                block_flag = memo.block_flag;
+                pass_flag = memo.password_needed_flag;
+                favorite_flag = memo.favorite_flag;
 
                 TitleView.setText(title);
                 TextView.setText(text);
             }
         });
+
+
 
         backBtn = findViewById(R.id.returnButton);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -66,17 +98,165 @@ public class MemoDisplay extends AppCompatActivity {
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MemoDisplay.this,MemoEdit.class);
+                Intent intent = new Intent(MemoDisplay.this,MemoImport.class);
                 intent.putExtra("KeyNUM", ID_NUM);
                 startActivity(intent);
             }
         });
     }
+//    public boolean onDoubleTap(MotionEvent e) {
+//        Toast.makeText(this, "called onDoubleTap()", Toast.LENGTH_LONG).show();
+//        return false;
+//    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, view, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.display_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item){
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        long itemId = item.getItemId();
+        Log.v("MY_LOG", "itemId:"+String.valueOf(itemId));
+//        mRealm.executeTransactionAsync(new Realm.Transaction() {
+//            @Override
+//            public void execute(Realm realm) {
+//                Memo memo = realm.where(Memo.class)
+//                        .equalTo("memo_id",ID_NUM)
+//                        .findFirst();
+//                memoBlock = memo.block_flag;
+//            }
+//        });
+        if(itemId == R.id.context_favorite) {
+            FavoriteAccept(ID_NUM);
+//        } else if (itemId == R.id.context_category) {
+        } else if (itemId == R.id.context_protect) {
+            BlockAccept(ID_NUM);
+        } else if (itemId == R.id.context_password) {
+            PassAccept(ID_NUM);
+        } else if (itemId == R.id.context_delete) {
+            if (block_flag == unflag){
+                DialogFragment dialogFragment = new DisplayDialogFragment();
+                Bundle args = new Bundle();
+//                        memoList.get();
+                args.putString("TITLE",title);
+                args.putString("TEXT",text);
+                args.putLong("KeyID",ID_NUM);
+                dialogFragment.setArguments(args);
+
+                dialogFragment.setCancelable(false);
+
+                dialogFragment.show(getSupportFragmentManager(),"Display_dialog");
+            }else {
+                Toast.makeText(MemoDisplay.this,R.string.Block_memo,Toast.LENGTH_LONG).show();
+            }
+        } else{
+            return super.onContextItemSelected(item);
+        }
+        return true;
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
         mRealm.close();
+    }
+    public void FavoriteAccept(long ID_NUM) {
+        mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Memo memo = realm.where(Memo.class)
+                        .equalTo("memo_id",ID_NUM)
+                        .findFirst();
+//                favorite_flag = memo.favorite_flag;
+                if (favorite_flag == unflag){
+                    memo.favorite_flag = flag;
+                }else {
+                    memo.favorite_flag = unflag;
+                }
+                favorite_flag = memo.favorite_flag;
+            }
+        });
+        if (favorite_flag == unflag){
+            Toast.makeText(MemoDisplay.this,R.string.Favorite_message,Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(MemoDisplay.this,R.string.Unfavorite_message,Toast.LENGTH_LONG).show();
+        }
+//        Intent intent = new Intent(MemoDisplay.this,MainActivity.class);
+//        startActivity(intent);
+    }
+    public void BlockAccept(long ID_NUM) {
+        Log.v("MY_LOG","test:"+title);
+        mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Memo memo = realm.where(Memo.class)
+                        .equalTo("memo_id",ID_NUM)
+                        .findFirst();
+//                block_flag = memo.block_flag;
+                if (block_flag == unflag){
+                    memo.block_flag = flag;
+//                    Toast.makeText(MemoDisplay.this,R.string.Block_message,Toast.LENGTH_LONG).show();
+                }else {
+                    memo.block_flag = unflag;
+//                    Toast.makeText(MemoDisplay.this,R.string.Unblock_message,Toast.LENGTH_LONG).show();
+                }
+                block_flag = memo.block_flag;
+            }
+        });
+        if (block_flag == 0){
+            Toast.makeText(MemoDisplay.this,R.string.Block_message,Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(MemoDisplay.this,R.string.Unblock_message,Toast.LENGTH_LONG).show();
+        }
+//        Intent intent = new Intent(MemoDisplay.this,MainActivity.class);
+//        startActivity(intent);
+    }
+
+    public void PassAccept(long ID_NUM) {
+        mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Memo memo = realm.where(Memo.class)
+                        .equalTo("memo_id",ID_NUM)
+                        .findFirst();
+//                pass_flag = memo.password_needed_flag;
+                if (pass_flag == unflag){
+                    memo.password_needed_flag = flag;
+//                    Toast.makeText(MemoDisplay.this,R.string.Pass_needed_message,Toast.LENGTH_LONG).show();
+                }else {
+                    memo.password_needed_flag = unflag;
+//                    Toast.makeText(MemoDisplay.this,R.string.Pass_unneeded_message,Toast.LENGTH_LONG).show();
+                }
+                pass_flag = memo.password_needed_flag;
+            }
+        });
+        if (pass_flag == 0){
+            Toast.makeText(MemoDisplay.this,R.string.Pass_needed_message,Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(MemoDisplay.this,R.string.Pass_unneeded_message,Toast.LENGTH_LONG).show();
+        }
+//        Intent intent = new Intent(MemoDisplay.this,MainActivity.class);
+//        startActivity(intent);
+    }
+    public void DeleteAccept(long ID_NUM) {
+        mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Memo memo = realm.where(Memo.class)
+                        .equalTo("memo_id",ID_NUM)
+                        .findFirst();
+
+                memo.deleteFromRealm();
+            }
+        });
+        Intent intent = new Intent(MemoDisplay.this,MainActivity.class);
+        startActivity(intent);
     }
 }
 
--- app/src/main/java/com/example/clipmemoapp/MemoImport.java
+++ app/src/main/java/com/example/clipmemoapp/MemoImport.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import java.util.Date;
 
@@ -19,10 +20,16 @@ import io.realm.Realm;
 public class MemoImport extends AppCompatActivity {
     Realm mRealm;
     private Button backBtn;
+    private Button saveBtn;
     private EditText I_Title;
     private EditText I_Text;
     private String G_Title;
     private String G_Text;
+    private String C_Title;
+    private String C_Text;
+    private String D_Title = "タイトル";
+    private String D_Text = "テキスト";
+    private String D_Memo = "タイトルとテキスト";
     private long newId = 0;
     private long ID_NUM;
 
@@ -51,6 +58,8 @@ public class MemoImport extends AppCompatActivity {
                             .equalTo("memo_id",ID_NUM).findFirst();
                     G_Title = memo.title;
                     G_Text = memo.text;
+                    C_Title = memo.title;
+                    C_Text = memo.text;
 
                     I_Title.setText(G_Title);
                     I_Text.setText(G_Text);
@@ -67,66 +76,241 @@ public class MemoImport extends AppCompatActivity {
                 G_Title = I_Title.getText().toString();
                 G_Text = I_Text.getText().toString();
 
-                mRealm.executeTransactionAsync(new Realm.Transaction() {
-                    @Override
-                    public void execute(Realm realm) {
-                        if (ID_NUM < 0){
-                            Number max = realm.where(Memo.class).max("memo_id");
-                            newId = 0;
-                            if (max != null){
-                                newId = max.longValue() + 1;
-                            }
-                            Memo memo
-                                    = realm.createObject(Memo.class, newId);
-                            if (G_Title.isEmpty()){
-                                memo.title = "No Title";
-                            }else {
-                                memo.title = G_Title;
-                            }
-                            if (G_Text.isEmpty()){
-                                memo.text = "No Text";
-                            }else {
-                                memo.text = G_Text;
-                            }
-                            memo.category = 0;
-                            memo.favorite_flag = 0;
-                            memo.block_flag = 0;
-                            memo.password_needed_flag = 0;
-                            memo.made_date = new Date();
-                            memo.updated_date = new Date();
-
-                            Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
-                            intent.putExtra("KeyNUM", newId);
-                            startActivity(intent);
-                        }else {
-                            Memo memo = realm.where(Memo.class)
-                                    .equalTo("memo_id",ID_NUM)
-                                    .findFirst();
-                            if (G_Title.isEmpty()){
-                                memo.title = "No Title";
-                            }else {
-                                memo.title = G_Title;
-                            }
-                            if (G_Text.isEmpty()){
-                                memo.text = "No Text";
-                            }else {
-                                memo.text = G_Text;
-                            }
-                            Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
-                            intent.putExtra("KeyNUM", ID_NUM);
-                            startActivity(intent);
-                        }
-
+                if (G_Title.isBlank()&&G_Text.isBlank()){
+                    if (ID_NUM < 0){
+                        Intent intent = new Intent(MemoImport.this,MainActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
+                        intent.putExtra("KeyNUM", ID_NUM);
+                        startActivity(intent);
                     }
-                });
+                }else {
+                    if (ID_NUM < 0){
+                        if (!G_Title.isBlank()&&!G_Text.isBlank()){
+                            CancelDialog(D_Memo);
+                        } else if (!G_Title.isBlank()||!G_Text.isBlank()){
+                            if (!G_Title.isBlank()){
+                                CancelDialog(D_Title);
+                            }else{
+                                CancelDialog(D_Text);
+                            }
+                        }
+//                        ImportAccept();
+                    }else {
+//                        if (G_Title.isBlank()||G_Text.isBlank()){
+//                            unEditAccept(ID_NUM);
+//                        }else {
+//                            if (!G_Title.equals(C_Title)||!G_Text.equals(C_Text)){
+//                                if (!G_Title.equals(C_Title)&&!G_Text.equals(C_Text)){
+//                                    EditDialog(D_Memo,ID_NUM);
+//                                } else if (!G_Title.equals(C_Title)){
+//                                    EditDialog(D_Title,ID_NUM);
+//                                }else{
+//                                    EditDialog(D_Text,ID_NUM);
+//                                }
+//                            }else {
+//                                unEditAccept(ID_NUM);
+//                            }
+//                        }
+                        if (!G_Title.equals(C_Title)||!G_Text.equals(C_Text)){
+                            if (G_Title.isBlank()||G_Text.isBlank()){
+                                if (G_Title.isBlank()&&G_Text.isBlank()){
+                                    BackDialog(D_Memo,ID_NUM);
+                                } else if (G_Title.isBlank()){
+                                    BackDialog(D_Title,ID_NUM);
+                                }else{
+                                    BackDialog(D_Text,ID_NUM);
+                                }
+                            }else {
+                                if (!G_Title.equals(C_Title)&&!G_Text.equals(C_Text)){
+                                    EditDialog(D_Memo,ID_NUM);
+                                } else if (!G_Title.equals(C_Title)){
+                                    EditDialog(D_Title,ID_NUM);
+                                }else{
+                                    EditDialog(D_Text,ID_NUM);
+                                }
+                            }
+                        }else {
+                            IntentAccept(ID_NUM);
+                        }
+                    }
+                }
             }
         });
-    }
 
+        saveBtn = findViewById(R.id.saveButton);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                G_Title = I_Title.getText().toString();
+                G_Text = I_Text.getText().toString();
+                if (ID_NUM < 0){
+                    if (!G_Title.isBlank()||!G_Text.isBlank()){
+                        if (!G_Title.isBlank()&&!G_Text.isBlank()){
+                            ImportAccept();
+                        } else if (!G_Title.isBlank()) {
+                            ImportDialog(D_Title,D_Text);
+                        }else {
+                            ImportDialog(D_Text,D_Title);
+                        }
+                    }else {
+                        unImportAccept();
+                    }
+                }else {
+                    if (!G_Title.isBlank()&&!G_Text.isBlank()){
+                        EditAccept(ID_NUM);
+                    } else if (!G_Title.isBlank()||!G_Text.isBlank()){
+                        if (!G_Title.isBlank()){
+                            BlankDialog(D_Text,ID_NUM);
+                        }else{
+                            BlankDialog(D_Title,ID_NUM);
+                        }
+                    }else {
+                        IntentAccept(ID_NUM);
+                    }
+                }
+            }
+        });
+
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
         mRealm.close();
+    }
+
+    public void CancelDialog(String ImpString){
+        DialogFragment dialogFragment = new CancelDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("TITLE",ImpString);
+        args.putString("TEXT",ImpString);
+        dialogFragment.setArguments(args);
+
+//        dialogFragment.setCancelable(false);
+
+        dialogFragment.show(getSupportFragmentManager(),"Cancel_dialog");
+    }
+
+    public void ImportDialog(String title,String text){
+        DialogFragment dialogFragment = new ImportDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("TITLE",title);
+        args.putString("TEXT",text);
+        dialogFragment.setArguments(args);
+
+        dialogFragment.setCancelable(false);
+
+        dialogFragment.show(getSupportFragmentManager(),"Import_dialog");
+    }
+
+    public void EditDialog(String EditString,long ID_NUM){
+        DialogFragment dialogFragment = new EditDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("TITLE",EditString);
+        args.putString("TEXT",EditString);
+        args.putLong("KeyID",ID_NUM);
+        dialogFragment.setArguments(args);
+
+//        dialogFragment.setCancelable(false);
+
+        dialogFragment.show(getSupportFragmentManager(),"Edit_dialog");
+    }
+
+    public void BlankDialog(String EditString,long ID_NUM){
+        DialogFragment dialogFragment = new BlankDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("TITLE",EditString);
+        args.putString("TEXT",EditString);
+        args.putLong("KeyID",ID_NUM);
+        dialogFragment.setArguments(args);
+
+//        dialogFragment.setCancelable(false);
+
+        dialogFragment.show(getSupportFragmentManager(),"Blank_dialog");
+    }
+
+    public void BackDialog(String EditString,long ID_NUM){
+        DialogFragment dialogFragment = new BackDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("TITLE",EditString);
+        args.putString("TEXT",EditString);
+        args.putLong("KeyID",ID_NUM);
+        dialogFragment.setArguments(args);
+
+//        dialogFragment.setCancelable(false);
+
+        dialogFragment.show(getSupportFragmentManager(),"Back_dialog");
+    }
+
+    public void ImportAccept() {
+        G_Title = I_Title.getText().toString();
+        G_Text = I_Text.getText().toString();
+        mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Number max = realm.where(Memo.class).max("memo_id");
+                newId = 0;
+                if (max != null){
+                    newId = max.longValue() + 1;
+                }
+                Memo memo
+                        = realm.createObject(Memo.class, newId);
+                if (G_Title.isBlank()){
+                    memo.title = "No Title";
+                }else {
+                    memo.title = G_Title;
+                }
+                if (G_Text.isBlank()){
+                    memo.text = "No Text";
+                }else {
+                    memo.text = G_Text;
+                }
+                memo.category = 0;
+                memo.favorite_flag = 0;
+                memo.block_flag = 0;
+                memo.password_needed_flag = 0;
+                memo.made_date = new Date();
+                memo.updated_date = new Date();
+
+                Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
+                intent.putExtra("KeyNUM", newId);
+                startActivity(intent);
+            }
+        });
+    }
+    public void EditAccept(long ID_NUM) {
+        G_Title = I_Title.getText().toString();
+        G_Text = I_Text.getText().toString();
+        mRealm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Memo memo = realm.where(Memo.class)
+                        .equalTo("memo_id",ID_NUM)
+                        .findFirst();
+                if (G_Title.isBlank()){
+                    memo.title = "No Title";
+                }else {
+                    memo.title = G_Title;
+                }
+                if (G_Text.isBlank()){
+                    memo.text = "No Text";
+                }else {
+                    memo.text = G_Text;
+                }
+            }
+        });
+        IntentAccept(ID_NUM);
+    }
+
+    public void IntentAccept(long ID_NUM) {
+        Intent intent = new Intent(MemoImport.this,MemoDisplay.class);
+        intent.putExtra("KeyNUM", ID_NUM);
+        startActivity(intent);
+    }
+    public void unImportAccept(){
+        Intent intent = new Intent(MemoImport.this,MainActivity.class);
+        startActivity(intent);
     }
 }
